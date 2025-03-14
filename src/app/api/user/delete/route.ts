2dfa66c232842018efeeaa3869540bf2404f21c8
Vilém Barnet <barnetv7@gmail.com>
2025-03-14 18:24:23 +0100

    Deleting users authors  when he deletes acc

--- src/app/api/user/delete/route.ts
+++ src/app/api/user/delete/route.ts
@@ -41,24 +41,52 @@ export async function DELETE() {
     mongoSession.startTransaction();
 
     try {
-      // 1. Delete all user's books (which also deletes embedded notes)
-      // Log the number of books deleted for audit purposes
-      const booksDeleteResult = await db.collection("books").deleteMany(
-        {
-          userId: userId.toString(),
-        },
-        { session: mongoSession }
-      );
+      // First, get the list of books for this user
+      // This helps us identify which author summaries are linked to the user's books
+      const userBooks = await db
+        .collection("books")
+        .find(
+          { userId: userId.toString() },
+          { projection: { author: 1, authorId: 1 } }
+        )
+        .toArray();
+
+      console.log(`Found ${userBooks.length} books for user ${userId}`);
+
+      // Extract author IDs from the user's books
+      const authorIds = userBooks
+        .filter((book) => book.authorId)
+        .map((book) => book.authorId);
+
+      // If we have author IDs, delete those author documents
+      if (authorIds.length > 0) {
+        // Convert string IDs to ObjectId if necessary
+        const objectIdAuthorIds = authorIds.map((id) =>
+          typeof id === "string" ? new mongoose.Types.ObjectId(id) : id
+        );
+
+        const authorsDeleteResult = await db
+          .collection("authors")
+          .deleteMany(
+            { _id: { $in: objectIdAuthorIds } },
+            { session: mongoSession }
+          );
+
+        console.log(
+          `Deleted ${authorsDeleteResult.deletedCount} author summaries for user ${userId}`
+        );
+      }
+
+      // Delete all user's books (which also deletes embedded notes)
+      const booksDeleteResult = await db
+        .collection("books")
+        .deleteMany({ userId: userId.toString() }, { session: mongoSession });
 
       console.log(
         `Deleted ${booksDeleteResult.deletedCount} books for user ${userId}`
       );
 
-      // 2. Delete any Author documents created by this user (if applicable)
-      // This step depends on your data model - if authors are linked to users
-      // await db.collection("authors").deleteMany({ createdBy: userId }, { session: mongoSession });
-
-      // 3. Delete the user account
+      // Delete the user account
       const userResult = await User.findOneAndDelete(
         { email: userEmail },
         { session: mongoSession }
